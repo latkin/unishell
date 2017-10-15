5b45dc42252994a92b1201e3818880be88ccd00f
Lincoln Atkinson <latkin@users.noreply.github.com>
2017-10-15 13:36:24 -0700

    Add Get-UniBytes and Get-UniString

--- unishell.psm1
+++ unishell.psm1
@@ -541,8 +541,79 @@ function Get-UniCodepoint {
     }
 }
 
+function Get-UniBytes {
+    param(
+        [Parameter(Mandatory=$true, Position = 0, ValueFromPipeline = $true, ParameterSetName = 'string')]
+        [string[]]$InputString,
+        [Parameter(Mandatory=$true, Position = 0, ValueFromPipeline = $true, ParameterSetName = 'codepoint')]
+        [pscustomobject[]] $Codepoint,
+        [string] $Encoding = 'utf-8'
+    )
+    begin {
+        $encodingImpl = $allEncodings |? WebName -eq $Encoding
+        if(-not $encodingImpl){
+            Write-Error "Encoding $encoding not available"
+            return
+        }
+    }
+    process {
+        if($PSCmdlet.ParameterSetName -eq 'string'){
+            foreach($s in $inputString){
+                if($encodingImpl){
+                    $encodingImpl.GetBytes($s)
+                }
+            }
+        } elseif ($PSCmdlet.ParameterSetName -eq 'codepoint') {
+            foreach($c in $codepoint){
+                if($c.PSObject.typenames.contains('unishell.codepoint')){
+                    $c.$encoding
+                }
+            }
+        }
+    }
+}
+
+function Get-UniString {
+    [CmdletBinding(DefaultParameterSetName = 'bytes')]
+    param(
+        [Parameter(Mandatory=$true, Position = 0, ValueFromPipeline = $true, ParameterSetName = 'bytes')]
+        [byte[]]$Bytes,
+        [Parameter(ParameterSetName = 'bytes')]
+        [string] $Encoding = 'utf-8',
+        [Parameter(Mandatory=$true, Position = 0, ValueFromPipeline = $true, ParameterSetName = 'codepoint')]
+        [pscustomobject[]] $Codepoint
+    )
+    begin {
+        $byteBuffer = New-Object 'System.Collections.Generic.List[byte]'
+        $sb = [System.Text.StringBuilder]::new()
+        $encodingImpl = $allEncodings |? WebName -eq $Encoding
+        if((-not $encodingImpl) -and ($psCmdlet.ParameterSetName -eq 'bytes')){
+            Write-Error "Encoding $encoding not available"
+            return
+        }
+    }
+    process {
+        if($PSCmdlet.ParameterSetName -eq 'bytes'){
+            foreach($b in $bytes) {
+                $byteBuffer.Add($b)
+            }
+        } elseif($PSCmdlet.ParameterSetName -eq 'codepoint'){
+            foreach($c in $codepoint) {
+                $null = $sb.Append($c.Value)
+            }
+        }
+    }
+    end {
+        if($PSCmdlet.ParameterSetName -eq 'bytes'){
+            $encodingImpl.GetString($byteBuffer.ToArray())
+        } elseif($PSCmdlet.ParameterSetName -eq 'codepoint'){
+            $sb.ToString()
+        }
+    }
+}
+
 if (Get-Command 'Register-ArgumentCompleter' -ea 0) {
-    Register-ArgumentCompleter -CommandName 'Get-UniCodepoint' -ParameterName 'Encoding' -ScriptBlock {
+    Register-ArgumentCompleter -CommandName 'Get-UniCodepoint','Get-UniBytes','Get-UniString' -ParameterName 'Encoding' -ScriptBlock {
         param($commandName, $parameterName, $wordToComplete, $commandAst, $boundParameters)
 
         $quote = $null
@@ -562,4 +633,4 @@ if (Get-Command 'Register-ArgumentCompleter' -ea 0) {
     }
 }
 
-Export-ModuleMember -Function 'Get-UniCodepoint'
+Export-ModuleMember -Function 'Get-UniCodepoint','Get-UniBytes','Get-UniString'
